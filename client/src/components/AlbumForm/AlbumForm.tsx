7c64142b5cde75f143e2d628492a2cfda68f3a9b
sai <kskprasad96@gmail.com>
2021-03-20 22:29:50 +0530

    form is now functional

--- client/src/components/AlbumForm/AlbumForm.tsx
+++ client/src/components/AlbumForm/AlbumForm.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { useState } from 'react';
+import Select from 'react-select';
 import { useFetch } from '../../hooks/useFetch';
 import './albumFormStyles.scss';
 
@@ -7,10 +9,21 @@ interface ArtistData {
   name: string;
 }
 
-interface FormState {
+interface PostData {
   title: string;
   artistId: number;
 }
+
+interface SelectOption {
+  label: string;
+  value: number;
+}
+
+export const constructSelectOptions = (optionsList: ArtistData[]) =>
+  optionsList.map(({ id, name }) => ({
+    label: name,
+    value: id,
+  }));
 
 function AlbumForm() {
   const {
@@ -19,9 +32,66 @@ function AlbumForm() {
     loading,
   } = useFetch<ArtistData[]>('/api/artists');
 
+  const [selectedArtist, setSelectedArtist] = useState<SelectOption | null>(
+    null
+  );
+  const [title, setTitle] = useState<string>('');
+
+  const createAlbum = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const res = await fetch('/api/new_album', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title, artistId: selectedArtist?.value }),
+    });
+
+    if (res.status === 200) {
+      window.location.href = '/';
+    } else {
+      console.log('error');
+    }
+  };
+
+  if (error) {
+    return <h2 className="error">{error}</h2>;
+  }
+
   return (
     <div className="album-form-page">
-      <h2>FOrm Hre</h2>
+      <h2>Create New Album</h2>
+      <form onSubmit={createAlbum} className="card">
+        <div className="form-row">
+          <label htmlFor="album-title">Title</label>
+          <input
+            type="text"
+            id="album-title"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+            placeholder="Enter Album Name..."
+            disabled={loading}
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="artist">Artist</label>
+          <Select
+            id="artist"
+            className="react-select-container"
+            classNamePrefix="react-select"
+            name="selectedArtist"
+            placeholder="Select Artist..."
+            options={artists !== null ? constructSelectOptions(artists) : []}
+            value={selectedArtist}
+            onChange={option => setSelectedArtist(option)}
+            isDisabled={loading}
+            isClearable={false}
+            isSearchable
+          />
+        </div>
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 }
