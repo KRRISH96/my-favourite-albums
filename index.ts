36b07d9220a8195c26882cb2e7a018e734cf0558
sai <kskprasad96@gmail.com>
2021-03-20 15:58:03 +0530

    setup endpoit for album creation and change root to /api namespace

--- index.ts
+++ index.ts
@@ -8,10 +8,34 @@ const prisma = new PrismaClient();
 app.use(bodyParser.json());
 
 
-app.get('/', async (_req, res) => {
+app.get('/api', async (_req, res) => {
   const albums = await prisma.album.findMany();
 
   res.json({ albums });
+});
+
+app.post('/api/new_album', async (req, res) => {
+  try {
+    const { title, artistId } = req.body;
+    if (!title || !artistId) {
+      res.status(400).json({ error: "Missing title or artistId" })
+      return;
+    }
+
+    const newAlbum = await prisma.album.create({
+      data: {
+        title,
+        artist: {
+          connect: { id: artistId },
+        },
+      },
+    });
+
+    res.status(200).json(newAlbum);
+  } catch (e) {
+    console.log(e); // Report to Error Service, ex: Airbrake
+    res.status(400).json({ error: "Sorry, Unabel to create an album at the moment! Please try again later" });
+  }
 })
 
 
