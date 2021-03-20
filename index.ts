01b87791be8d948cee3453b0cddbcc919d7ad14d
sai <kskprasad96@gmail.com>
2021-03-20 21:14:43 +0530

    prettier

--- index.ts
+++ index.ts
@@ -5,13 +5,12 @@ import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const prisma = new PrismaClient();
- 
-app.use(bodyParser.json());
 
+app.use(bodyParser.json());
 
 app.get('/api', async (_req, res) => {
   const albums = await prisma.album.findMany({
-    include: { artist: true }
+    include: { artist: true },
   });
 
   res.json(albums);
@@ -27,7 +26,7 @@ app.post('/api/new_album', async (req, res) => {
   try {
     const { title, artistId } = req.body;
     if (!title || !artistId) {
-      res.status(400).json({ error: "Missing title or artistId" })
+      res.status(400).json({ error: 'Missing title or artistId' });
       return;
     }
 
@@ -43,19 +42,23 @@ app.post('/api/new_album', async (req, res) => {
     res.status(200).json(newAlbum);
   } catch (e) {
     console.log(e); // Report to Error Service, ex: Airbrake
-    res.status(400).json({ error: "Sorry, Unabel to create an album at the moment! Please try again later" });
+    res
+      .status(400)
+      .json({
+        error:
+          'Sorry, Unabel to create an album at the moment! Please try again later',
+      });
   }
-})
+});
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build/')));
 
-  app.get('*', function(_req, res) {
+  app.get('*', function (_req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
+const PORT = process.env.PORT || 4000;
 
-const PORT = process.env.PORT || 4000
- 
 app.listen(PORT, () => console.log(`Running at: http://localhost:${PORT}`));
